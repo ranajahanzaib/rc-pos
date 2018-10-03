e13429cacc4d1c69586fe3782dc7f99b78e7e071
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-03 17:46:00 +0500

    set up Gulp Tasks

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss', 'node_modules/font-awesome/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
+
+
+// Move the javascript files into our /src/js folder
+gulp.task('js', function() {
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.stream());
+});
+
+
+// Move the fonts files into our /src/fonts folder
+gulp.task('fonts', function() {
+  return gulp.src(['node_modules/font-awesome/fonts/*.eot', 'node_modules/font-awesome/fonts/*.svg', 'node_modules/font-awesome/fonts/*.woff'])
+    .pipe(gulp.dest("src/fonts"))
+    .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: "src/"
+  });
+
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+  gulp.watch("src/*.html").on('change', browserSync.reload);
+})
+
+
+gulp.task('default', ['fonts','js','serve']);
