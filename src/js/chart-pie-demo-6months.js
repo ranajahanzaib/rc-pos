4aed8df9caec188f38238d919abf4b3bbbe2f2ff
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-06 22:43:24 +0500

    Added Top Selling Stats

--- src/js/chart-pie-demo-6months.js
+++ src/js/chart-pie-demo-6months.js
@@ -3,13 +3,13 @@ Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSyste
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
 // Pie Chart Example
-var ctx = document.getElementById("myPieChart");
+var ctx = document.getElementById("myPieChart6Months");
 var myPieChart = new Chart(ctx, {
   type: 'pie',
   data: {
     labels: ["Routers", "Chargers & Cables", "USB Storage", "Display Monitors", "CPU", "Speakers", "Mouse & Pointing Devices", "Power Supplies", "Windows Installation", "Linux Installation", "Others"],
     datasets: [{
-      data: [2.21, 4.58, 8.25, 16.32, 32.32, 64.32, 1.32, 2.32, 3.32, 4.32, 5.32],
+      data: [5.21, 9.58, 11.25, 1.32, 2.32, 4.32, 11.32, 24.32, 30.32, 14.32, 5.32],
       backgroundColor: ['#d35400', '#00a63f', '#900C3F', '#2e4053', '#1a5276', '#f4d03f','#8e44ad','#DAF7A6','#34eb13','#eb1d13','#0a0101'],
     }],
   },
